60df3c3293dbcf56bc73e46c8230ed6530248ff6
Matas Žilinskas <zilinskas.matas1999@gmail.com>
2021-02-11 18:39:22 +0200

    Added initial project + EF setup

---
+++ TutoringApp/TutoringApp/Configurations/StartupExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TutoringApp.Data;
+
+namespace TutoringApp.Configurations
+{
+    public static class StartupExtensions
+    {
+        public static void SetUpDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(o => o.UseSqlServer(connectionString));
+
+            services
+                .BuildServiceProvider()
+                .GetService<ApplicationDbContext>()?
+                .Database.Migrate();
+        }
+    }
+}
---
+++ TutoringApp/TutoringApp/Data/Models/Base/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace TutoringApp.Data.Models.Base
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
