1ceea5861c05d5f477858f12bd4bd0bc740f24b4
Matas Žilinskas <zilinskas.matas1999@gmail.com>
2021-03-03 13:29:39 +0200

    Added migration

--- TutoringApp/Migrations/20210303112909_RemovedTutorRole.Designer.cs
+++ TutoringApp/Migrations/20210303112909_RemovedTutorRole.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TutoringApp.Data;
 
 namespace TutoringApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210303112909_RemovedTutorRole")]
+    partial class RemovedTutorRole
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TutoringApp/Migrations/20210303112909_RemovedTutorRole.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TutoringApp.Migrations
+{
+    public partial class RemovedTutorRole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "48b3c783-707f-46ea-9349-89a051066cc5");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "48b3c783-707f-46ea-9349-89a051066cc5", "698a1fe5-0e2d-40db-bab2-7c6af6b24288", "Tutor", "TUTOR" });
+        }
+    }
+}
