ed0dcc0d72fabf1cb42b0883bbc2100ddaae1196
Matas Žilinskas <zilinskas.matas1999@gmail.com>
2021-03-03 15:01:33 +0200

    Added migration

---
+++ TutoringApp/Migrations/20210303130042_RenamedTutoringRequests.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TutoringApp.Migrations
+{
+    public partial class RenamedTutoringRequests : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TutoringRequests");
+
+            migrationBuilder.DropColumn(
+                name: "IsTutor",
+                table: "AspNetUsers");
+
+            migrationBuilder.CreateTable(
+                name: "TutoringApplications",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    ModuleId = table.Column<int>(type: "int", nullable: false),
+                    RequestDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    MotivationalLetter = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TutoringApplications", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TutoringApplications_AspNetUsers_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_TutoringApplications_Modules_ModuleId",
+                        column: x => x.ModuleId,
+                        principalTable: "Modules",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TutoringApplications_ModuleId_StudentId",
+                table: "TutoringApplications",
+                columns: new[] { "ModuleId", "StudentId" },
+                unique: true,
+                filter: "[StudentId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TutoringApplications_StudentId",
+                table: "TutoringApplications",
+                column: "StudentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TutoringApplications");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsTutor",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "TutoringRequests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RequestDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    StatusChangeDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    StudentId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TutoringRequests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TutoringRequests_AspNetUsers_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TutoringRequests_StudentId",
+                table: "TutoringRequests",
+                column: "StudentId");
+        }
+    }
+}
