0411f0d32b61cf2861afc2c7b57e5d619d2e60e4
Matas Žilinskas <zilinskas.matas1999@gmail.com>
2021-03-11 16:43:51 +0200

    Added migration

---
+++ TutoringApp/Migrations/20210311144247_StudentTutorModuleScope.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TutoringApp.Migrations
+{
+    public partial class StudentTutorModuleScope : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_StudentTutors_StudentId_TutorId",
+                table: "StudentTutors");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ModuleId",
+                table: "StudentTutors",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentTutors_ModuleId",
+                table: "StudentTutors",
+                column: "ModuleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentTutors_StudentId_TutorId_ModuleId",
+                table: "StudentTutors",
+                columns: new[] { "StudentId", "TutorId", "ModuleId" },
+                unique: true,
+                filter: "[StudentId] IS NOT NULL AND [TutorId] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudentTutors_Modules_ModuleId",
+                table: "StudentTutors",
+                column: "ModuleId",
+                principalTable: "Modules",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudentTutors_Modules_ModuleId",
+                table: "StudentTutors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_StudentTutors_ModuleId",
+                table: "StudentTutors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_StudentTutors_StudentId_TutorId_ModuleId",
+                table: "StudentTutors");
+
+            migrationBuilder.DropColumn(
+                name: "ModuleId",
+                table: "StudentTutors");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentTutors_StudentId_TutorId",
+                table: "StudentTutors",
+                columns: new[] { "StudentId", "TutorId" },
+                unique: true,
+                filter: "[StudentId] IS NOT NULL AND [TutorId] IS NOT NULL");
+        }
+    }
+}
