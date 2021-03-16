746ef98a1f0174fd8e20286f99e8bd39a9c56537
Matas Žilinskas <zilinskas.matas1999@gmail.com>
2021-03-16 18:21:30 +0200

    Frontend service and models

---
+++ TutoringApp/ClientApp/src/app/models/chats/chat-message-new.ts
@@ -0,0 +1,3 @@
+export interface ChatMessageNew {
+  content: string;
+}
---
+++ TutoringApp/ClientApp/src/app/services/chats/chats.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ChatMessage } from 'src/app/models/chats/chat-message';
+import { ChatMessageNew } from 'src/app/models/chats/chat-message-new';
+import { HttpService } from '../http.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatsService {
+  private usersController = 'Users';
+
+  constructor(
+    private httpService: HttpService
+  ) { }
+
+  public postChatMessage(receiverId: string, chatMessage: ChatMessageNew): Observable<ChatMessage> {
+    return this.httpService.post(this.usersController, `${receiverId}/chat-messages`, chatMessage);
+  }
+
+  public getChatMessages(receiverId: string): Observable<ChatMessage[]> {
+    return this.httpService.get(this.usersController, `${receiverId}/chat-messages`);
+  }
+}
