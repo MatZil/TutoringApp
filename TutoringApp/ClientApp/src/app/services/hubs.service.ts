5bcbfe560891bdb704b1c5887c6f166d83dbb80c
Matas Žilinskas <zilinskas.matas1999@gmail.com>
2021-03-20 14:01:33 +0200

    Installed microsoft signalr

--- TutoringApp/ClientApp/src/app/services/hubs.service.ts
+++ TutoringApp/ClientApp/src/app/services/hubs.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import * as signalR from '@aspnet/signalr';
+import * as signalR from '@microsoft/signalr';
 import { filter, tap } from 'rxjs/operators';
 import { ChatMessage } from '../models/chats/chat-message';
 import { AuthService } from './auth/auth.service';
