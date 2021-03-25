63a1523a4139bd7965cd09f726bcc055851decb2
Matas Žilinskas <zilinskas.matas1999@gmail.com>
2021-03-25 17:11:36 +0200

    Added migration

---
+++ TutoringApp/Migrations/20210325151039_AddedModuleToTutoringSession.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TutoringApp.Migrations
+{
+    public partial class AddedModuleToTutoringSession : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ModuleId",
+                table: "TutoringSessions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TutoringSessions_ModuleId",
+                table: "TutoringSessions",
+                column: "ModuleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TutoringSessions_Modules_ModuleId",
+                table: "TutoringSessions",
+                column: "ModuleId",
+                principalTable: "Modules",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TutoringSessions_Modules_ModuleId",
+                table: "TutoringSessions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TutoringSessions_ModuleId",
+                table: "TutoringSessions");
+
+            migrationBuilder.DropColumn(
+                name: "ModuleId",
+                table: "TutoringSessions");
+        }
+    }
+}
