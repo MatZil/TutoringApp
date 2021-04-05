fe69e72c4761c8bd1c77d0e5624c10cccbb75282
Matas Žilinskas <zilinskas.matas1999@gmail.com>
2021-04-05 12:14:52 +0300

    Added migration

---
+++ TutoringApp/Migrations/20210405091430_AddedCancellationReason.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TutoringApp.Migrations
+{
+    public partial class AddedCancellationReason : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CancellationReason",
+                table: "TutoringSessions",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CancellationReason",
+                table: "TutoringSessions");
+        }
+    }
+}
