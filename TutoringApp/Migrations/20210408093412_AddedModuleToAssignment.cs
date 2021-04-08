ab1e614677f659eeb56cc84863aedeaf12befb5b
Matas Žilinskas <zilinskas.matas1999@gmail.com>
2021-04-08 12:34:28 +0300

    Added migration

---
+++ TutoringApp/Migrations/20210408093412_AddedModuleToAssignment.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TutoringApp.Migrations
+{
+    public partial class AddedModuleToAssignment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Comment",
+                table: "Assignments");
+
+            migrationBuilder.DropColumn(
+                name: "Content",
+                table: "Assignments");
+
+            migrationBuilder.DropColumn(
+                name: "SubmissionFileName",
+                table: "Assignments");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Assignments");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ModuleId",
+                table: "Assignments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Assignments_ModuleId",
+                table: "Assignments",
+                column: "ModuleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Assignments_Modules_ModuleId",
+                table: "Assignments",
+                column: "ModuleId",
+                principalTable: "Modules",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Assignments_Modules_ModuleId",
+                table: "Assignments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Assignments_ModuleId",
+                table: "Assignments");
+
+            migrationBuilder.DropColumn(
+                name: "ModuleId",
+                table: "Assignments");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Comment",
+                table: "Assignments",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Content",
+                table: "Assignments",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SubmissionFileName",
+                table: "Assignments",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Assignments",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
