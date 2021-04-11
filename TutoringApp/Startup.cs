c5bbc4882eba0b46d28bb4b2180bccd7f5653c25
Matas Žilinskas <zilinskas.matas1999@gmail.com>
2021-04-11 12:42:27 +0300

    Specify client app root path

--- TutoringApp/Startup.cs
+++ TutoringApp/Startup.cs
@@ -31,6 +31,12 @@ namespace TutoringApp
             services.ConfigureDependencyInjections();
             services.AddSignalR(o => o.EnableDetailedErrors = true);
             services.AddControllers();
+
+            // In production, the Angular files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
