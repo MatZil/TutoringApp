0df1cba98ad0a894e8835c213ee5871056b0649b
Matas Žilinskas <zilinskas.matas1999@gmail.com>
2021-04-11 14:25:59 +0300

    use spa static files

--- TutoringApp/Startup.cs
+++ TutoringApp/Startup.cs
@@ -46,9 +46,19 @@ namespace TutoringApp
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            if (!env.IsDevelopment())
+            {
+                app.UseSpaStaticFiles();
+            }
 
             app.UseRouting();
 
