40cd6043d1f0e10a664436d134dfc979814a2a1f
Matas Žilinskas <zilinskas.matas1999@gmail.com>
2021-04-20 10:58:57 +0300

    Updated migrations

---
+++ TutoringApp/Migrations/20210420075843_RemovedLecturer.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TutoringApp.Migrations
+{
+    public partial class RemovedLecturer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "d2365ab4-0cf5-48ae-8216-b3f28cf4cd9f");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "d2365ab4-0cf5-48ae-8216-b3f28cf4cd9f", "781deac4-300c-40b4-84dc-d5e15697c752", "Lecturer", "LECTURER" });
+        }
+    }
+}
